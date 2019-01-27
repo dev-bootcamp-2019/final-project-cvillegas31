c186fd38a69386ad6947f018c64b4fd80f81f777
Carlos Andres Villegas Gomez <cvillegas31@gmail.com>
2019-01-27 20:14:29 +0000

    Unit tests

--- test/TestProofOfExistence.sol
+++ test/TestProofOfExistence.sol
@@ -4,20 +4,20 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/ProofOfExistence.sol";
 
-contract ProofOfExistence {
+contract TestProofOfExistence {
 
-  uint public initialBalance = 1 ether;
+  //uint public initialBalance = 1 ether;
 
-  function testRegisterFile() {
+  //function testRegisterFile() {
    
 
-    ProofOfExistence simpleStorage = ProofOfExistence(DeployedAddresses.ProofOfExistence());
+   // ProofOfExistence simpleStorage = ProofOfExistence(DeployedAddresses.ProofOfExistence());
 
     //simpleStorage.set(89);
 
     //uint expected = 89;
 
     //Assert.equal(simpleStorage.get(), expected, "It should store the value 89.");
-  }
+  //}
 
 }
---
+++ test/proofofexistece.js
@@ -0,0 +1,66 @@
+var ProofOfExistence = artifacts.require('./ProofOfExistence')
+const crypto = require("crypto");
+
+function sha256AsHexString(doc) {
+    return "0x" + crypto.createHash("sha256").update(doc, "utf8").digest("hex");
+}
+
+contract('ProofOfExistence', function(accounts) {
+
+    const owner = accounts[0]
+    const price = web3.toWei(1, "ether")
+
+    it("should register a filehash (happy path)", async() => {
+        const proofOfExistence = await ProofOfExistence.deployed()
+
+        const fileHash = sha256AsHexString("file")
+	const tx = await proofOfExistence.registerFile(fileHash)
+        const exists = await proofOfExistence.checkFileExists(fileHash)
+
+	assert.equal(exists, true, 'file does not stored')
+    })
+    it("should not register an existing asset (fail proof)", async() => {
+        const proofOfExistence = await ProofOfExistence.deployed()
+
+        var eventEmitted = false
+        const fileHash = sha256AsHexString("fileTest")
+	const tx = await proofOfExistence.registerFile(fileHash)
+      
+        try {
+              //Same file
+               
+	      const tx2 = await proofOfExistence.registerFile(fileHash)
+        } catch (error) {
+            return;
+        }
+        assert(false, 'Expected Error: "VM Exception while processing transaction: revert" not received');
+    })
+    it("only owner (fail proof)", async() => {
+        const proofOfExistence = await ProofOfExistence.deployed()
+        const fileHash = "fileTest3"
+	try {
+	   const tx2 = await proofOfExistence.registerFile(fileHash, {from: accounts[1]})
+           console.log(tx2)
+        } catch (error) {
+             console.log("Entra aqui")
+            return;
+        }
+        assert(false, 'Expected Error: "VM Exception while processing transaction: revert" not received');
+    })
+    it("files by user/owner (happy path)", async() => {
+        const proofOfExistence = await ProofOfExistence.deployed()
+
+        //previously has been stored two files
+        const result = await proofOfExistence.getUserMap(owner)
+
+        assert.equal(result.length, 2, 'There are more or less than two for this user')
+     
+    })
+    it("files by user/not-owner (happy path)", async() => {
+        const proofOfExistence = await ProofOfExistence.deployed()
+
+        //it has not been stored files for the user accounts[1]
+        const result = await proofOfExistence.getUserMap(accounts[1])
+        assert.equal(result.length, 0, 'There are files for this user')     
+    })
+});
